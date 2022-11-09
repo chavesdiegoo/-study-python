d14a849665d581f4ddffe66adc87a080b9e046b2
chavesdiegoo <79767758+chavesdiegoo@users.noreply.github.com>
2022-11-09 09:23:35 -0300

    Exercicios estrutura sequencial Python Brasil

---
+++ PythonBrasil/5.py
@@ -0,0 +1,3 @@
+# 5) Faça um Programa que converta metros para centímetros.
+m = float(input('Imsira a distância em metros: '))
+print ('A distancia representa Centímetros: ', (m * 100))
---
+++ PythonBrasil/6.py
@@ -0,0 +1,4 @@
+ # 6) Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+r = float(input('Raio do círculo: '))
+a = 3.14 * (r ** 2)
+print ('A Área do círculo corresponde a: ', a)
